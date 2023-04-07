4cda4be1951aa151621ed9d98c69c72b231eb733
tunameatball <kh9768@gmail.com>
2023-04-07 23:52:49 +0900

    [ Android ] add webview to MainActivity.kt

--- android/webtoon_app/app/src/main/java/com/kkh/webtoon_app/MainActivity.kt
+++ android/webtoon_app/app/src/main/java/com/kkh/webtoon_app/MainActivity.kt
@@ -2,10 +2,18 @@ package com.kkh.webtoon_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val webView = findViewById<WebView>(R.id.web_view)
+        webView.webViewClient = WebViewClient()
+        webView.settings.javaScriptEnabled = true
+
+        webView.loadUrl("https://www.google.com")
     }
 }
