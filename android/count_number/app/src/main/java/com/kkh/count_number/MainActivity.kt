3e61647428bf6684fca12fa178344234ba9ceaf2
tunameatball <kh9768@gmail.com>
2023-04-07 21:03:25 +0900

    [ Android ] create count_number project

---
+++ android/count_number/app/src/main/java/com/kkh/count_number/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.kkh.count_number
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
