42118c28d4d0db3abde337f3d0fa41df64ea508d
tunameatball <kh9768@gmail.com>
2023-04-07 21:44:23 +0900

    [ Android ] Add counting function to count_number project

--- android/count_number/app/src/main/java/com/kkh/count_number/MainActivity.kt
+++ android/count_number/app/src/main/java/com/kkh/count_number/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.kkh.count_number
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tvNumber = findViewById<TextView>(R.id.tv_number)
+
+        findViewById<Button>(R.id.btn_init).setOnClickListener {
+            tvNumber.text = "0"
+        }
+
+        findViewById<Button>(R.id.btn_plus).setOnClickListener {
+            var plusNumber = tvNumber.text.toString().toInt() + 1
+            tvNumber.text = plusNumber.toString()
+        }
     }
 }
