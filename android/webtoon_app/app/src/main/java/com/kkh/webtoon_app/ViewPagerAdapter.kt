4b927101d78e6f2fefdbfa37680c08c0bebf832a
tunameatball <kh9768@gmail.com>
2023-04-15 13:48:04 +0900

    [ Android ] Edit ViewPagerAdapter In createFragment() function

--- android/webtoon_app/app/src/main/java/com/kkh/webtoon_app/ViewPagerAdapter.kt
+++ android/webtoon_app/app/src/main/java/com/kkh/webtoon_app/ViewPagerAdapter.kt
@@ -5,33 +5,19 @@ import androidx.viewpager2.adapter.FragmentStateAdapter
 
 class ViewPagerAdapter(private val mainActivity: MainActivity) : FragmentStateAdapter(mainActivity) {
 
+    private val urls: List<String> = ArrayList<String>().apply {
+        add("https://comic.naver.com/webtoon/detail?titleId=783053&no=75&weekday=mon")
+        add("https://comic.naver.com/webtoon/detail?titleId=783053&no=74&weekday=mon")
+        add("https://comic.naver.com/webtoon/detail?titleId=783053&no=73&weekday=mon")
+    }
+
     override fun getItemCount(): Int {
         return 3
     }
 
     override fun createFragment(position: Int): Fragment {
-        return when(position) {
-            0 -> {
-                WebViewFragment(position, "https://comic.naver.com/webtoon/detail?titleId=783053&no=75&weekday=mon").apply {
-                    listener = mainActivity
-                }
-
-            }
-            1 -> {
-                WebViewFragment(position, "https://comic.naver.com/webtoon/detail?titleId=783053&no=74&weekday=mon").apply {
-                    listener = mainActivity
-                }
-            }
-            2 -> {
-                WebViewFragment(position, "https://comic.naver.com/webtoon/detail?titleId=783053&no=73&weekday=mon").apply {
-                    listener = mainActivity
-                }
-            }
-            else -> {
-                WebViewFragment(position, "https://comic.naver.com/webtoon/detail?titleId=783053&no=1&weekday=mon").apply {
-                    listener = mainActivity
-                }
-            }
+        return WebViewFragment(position, urls[position]).apply {
+            listener = mainActivity
         }
     }
 
