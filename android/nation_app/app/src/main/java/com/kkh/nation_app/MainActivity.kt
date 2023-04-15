6d3ba13e571af9450458d98a0242d446ac318e0e
tunameatball <kh9768@gmail.com>
2023-04-15 16:24:24 +0900

    [ Android ] add viewBinding

--- android/nation_app/app/src/main/java/com/kkh/nation_app/MainActivity.kt
+++ android/nation_app/app/src/main/java/com/kkh/nation_app/MainActivity.kt
@@ -2,10 +2,16 @@ package com.kkh.nation_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kkh.nation_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
     }
 }
