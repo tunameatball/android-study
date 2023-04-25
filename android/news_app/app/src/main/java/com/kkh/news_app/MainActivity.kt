304f66954c60b7427a76ea69b057b5626ed9dc19
tunameatball <kh9768@gmail.com>
2023-04-25 19:50:00 +0900

    [ Android ] get RSS feed

--- android/news_app/app/src/main/java/com/kkh/news_app/MainActivity.kt
+++ android/news_app/app/src/main/java/com/kkh/news_app/MainActivity.kt
@@ -2,10 +2,41 @@ package com.kkh.news_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.tickaroo.tikxml.TikXml
+import com.tickaroo.tikxml.retrofit.TikXmlConverterFactory
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val BASE_URL = "https://news.google.com/"
+    }
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(TikXmlConverterFactory.create(
+            TikXml.Builder()
+                .exceptionOnUnreadXml(false)
+                .build()
+        )).build()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val newsService = retrofit.create(NewsService::class.java)
+        newsService.mainFeed().enqueue(object: Callback<NewsRss> {
+            override fun onResponse(call: Call<NewsRss>, response: Response<NewsRss>) {
+                Log.e("MainActivity", "${response.body()?.channel?.items}")
+            }
+
+            override fun onFailure(call: Call<NewsRss>, t: Throwable) {
+                t.printStackTrace()
+            }
+        })
     }
 }
