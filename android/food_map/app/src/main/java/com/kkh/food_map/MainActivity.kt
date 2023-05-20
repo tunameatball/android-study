7c8f9db1ff012caaf4ee1cb210bad472ee24cb69
tunameatball <kh9768@gmail.com>
2023-05-20 16:35:35 +0900

    [ Android ] 네이버 검색 후 지도에 표시

--- android/food_map/app/src/main/java/com/kkh/food_map/MainActivity.kt
+++ android/food_map/app/src/main/java/com/kkh/food_map/MainActivity.kt
@@ -2,13 +2,16 @@ package com.kkh.food_map
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView.OnQueryTextListener
 import com.kkh.food_map.databinding.ActivityMainBinding
-import com.naver.maps.geometry.LatLng
+import com.naver.maps.geometry.Tm128
 import com.naver.maps.map.CameraAnimation
 import com.naver.maps.map.CameraUpdate
 import com.naver.maps.map.NaverMap
 import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.map.overlay.Marker
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -27,16 +30,69 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
         binding.mapView.getMapAsync(this)
 
-        SearchRepository.getGoodRestaurant("서울").enqueue(object : Callback<SearchResult> {
-            override fun onResponse(call: Call<SearchResult>, response: Response<SearchResult>) {
-                Log.e("aa", response.body().toString())
+        binding.searchView.setOnQueryTextListener(object : OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+
+                return if (query?.isNotEmpty() == true) {
+                    SearchRepository.getGoodRestaurant(query)
+                        .enqueue(object : Callback<SearchResult> {
+                            override fun onResponse(
+                                call: Call<SearchResult>,
+                                response: Response<SearchResult>
+                            ) {
+                                val searchItemList = response.body()?.items.orEmpty()
+
+                                if (searchItemList.isEmpty()) {
+                                    Toast.makeText(
+                                        this@MainActivity,
+                                        "검색 결과가 없습니다",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                    return
+                                } else if (!this@MainActivity::naverMap.isInitialized) {
+                                    Toast.makeText(
+                                        this@MainActivity,
+                                        "오류가 발생했습니다",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                    return
+                                }
+
+                                val markers = searchItemList.map {
+                                    Marker(
+                                        Tm128(
+                                            it.mapx.toDouble(),
+                                            it.mapy.toDouble()
+                                        ).toLatLng()
+                                    ).apply {
+                                        captionText = it.title
+                                        map = naverMap
+                                        width = 50
+                                        height = 70
+                                    }
+                                }
+
+                                val cameraUpdate = CameraUpdate.scrollTo(markers.first().position)
+                                    .animate(CameraAnimation.Easing)
+                                naverMap.moveCamera(cameraUpdate)
+
+                            }
+
+                            override fun onFailure(call: Call<SearchResult>, t: Throwable) {
+                                Log.e("onFailure", t.message ?: "error")
+                            }
+                        })
+                    false
+                } else {
+                    true
+                }
             }
 
-            override fun onFailure(call: Call<SearchResult>, t: Throwable) {
-                Log.e("onFailure", t.message ?: "error")
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return true
             }
+
         })
-
     }
 
     override fun onStart() {
@@ -76,8 +132,5 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     override fun onMapReady(p0: NaverMap) {
         naverMap = p0
-        val cameraUpdate = CameraUpdate.scrollTo(LatLng(37.6666102, 126.9783881))
-            .animate(CameraAnimation.Easing)
-        naverMap.moveCamera(cameraUpdate)
     }
 }
