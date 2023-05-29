79ad1b59e317208587e896437e5a94fb66c90c55
tunameatball <kh9768@gmail.com>
2023-05-29 15:26:10 +0900

    [ Android ] create share location project

---
+++ android/share_location/app/src/main/java/com/kkh/share_location/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.kkh.share_location
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
