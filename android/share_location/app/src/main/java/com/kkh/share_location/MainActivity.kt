2d13c7d48f662d371c1ec2dadc4e2b8806e3995a
tunameatball <kh9768@gmail.com>
2023-05-29 15:30:00 +0900

    [ Android ] ViewBinding 추가

--- android/share_location/app/src/main/java/com/kkh/share_location/MainActivity.kt
+++ android/share_location/app/src/main/java/com/kkh/share_location/MainActivity.kt
@@ -2,10 +2,13 @@ package com.kkh.share_location
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kkh.share_location.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
