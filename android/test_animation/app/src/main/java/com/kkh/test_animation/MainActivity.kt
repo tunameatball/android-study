5e821d37514109114f98c3a08c1831d180aebce7
tunameatball <kh9768@gmail.com>
2023-10-22 13:54:44 +0900

    init webview test project

--- android/test_animation/app/src/main/java/com/kkh/test_animation/MainActivity.kt
+++ android/test_animation/app/src/main/java/com/kkh/test_animation/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.kkh.test_animation
 
+import android.app.ActivityOptions
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.WindowManager
+import androidx.core.app.ActivityCompat
+import androidx.core.app.ActivityOptionsCompat
+import androidx.core.view.postDelayed
+import androidx.core.util.Pair
+import com.kkh.test_animation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(window) {
+            setFlags(
+            WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED,
+            WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED)
+        }
+
+        binding.centerLogo.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            Log.d("transitionName", binding.centerLogo.transitionName)
+            val options = ActivityOptions.makeSceneTransitionAnimation(this, binding.centerLogo, binding.centerLogo.transitionName)
+
+            startActivity(intent, options.toBundle())
+        }
+
+
     }
 }
