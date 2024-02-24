e3c4bb9571fc2e7320097e0d0f39a23750db2d55
tunameatball <kh9768@gmail.com>
2024-02-24 16:04:49 +0900

    Create Base Classes

---
+++ android-firebase/app/src/main/java/com/kkh/android_firebase/base/BaseActivity.kt
@@ -0,0 +1,7 @@
+package com.kkh.android_firebase.base
+
+import androidx.appcompat.app.AppCompatActivity
+
+open class BaseActivity : AppCompatActivity() {
+
+}
---
+++ android-firebase/app/src/main/java/com/kkh/android_firebase/base/BaseViewModel.kt
@@ -0,0 +1,6 @@
+package com.kkh.android_firebase.base
+
+import androidx.lifecycle.ViewModel
+
+open class BaseViewModel : ViewModel() {
+}
