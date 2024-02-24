c98d016a255f6c16ff806002e64d6f917a928ece
tunameatball <kh9768@gmail.com>
2024-02-24 16:46:39 +0900

    Crash Test

--- android-firebase/app/src/main/java/com/kkh/android_firebase/MainActivity.kt
+++ android-firebase/app/src/main/java/com/kkh/android_firebase/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.kkh.android_firebase
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.kkh.android_firebase.base.BaseActivity
+import com.kkh.android_firebase.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setClickListener()
+    }
+
+    private fun setClickListener() {
+        if (::binding.isInitialized) {
+            binding.btnCrash.setOnClickListener(this)
+        }
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.btn_crash -> {
+                throw Exception("Test Crash~~")
+            }
+        }
     }
 }
